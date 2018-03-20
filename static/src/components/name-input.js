ec09a80200a3ed9f43d86da9e9d2fa2829880fd8
Tanya <tbatsenko@gmail.com>
2018-03-20 13:35:01 +0200

    fix conflicts

--- static/src/components/name-input.js
+++ static/src/components/name-input.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import Select from 'react-select';
-=======
-import { Async } from 'react-select';
->>>>>>> 60176d8d40e2880452f1335ef857ee0afa4515e0
 
 export default class NameInput extends Component{
     state = {
@@ -11,74 +7,18 @@ export default class NameInput extends Component{
     }
     handleChange = (selectedOption) => {
       this.setState({selectedOption});
-<<<<<<< HEAD
     };
-=======
-      console.log(`Selected: ${selectedOption.label}`);
-      this.setState({medName});
-    }
-
-    _parseJSON = (response) => {
-      return response.text().then(function(text) {
-        console.log("Text: ", text);
-        return text ? JSON.parse(text) : {}
-      })
-    }
->>>>>>> 60176d8d40e2880452f1335ef857ee0afa4515e0
 
     render() {
         const { selectedOption } = this.state;
         const value = selectedOption;
-<<<<<<< HEAD
         return (
             <div>
               <Select
-=======
-        const url = 'http://10.10.120.131:5000/name_options';
-        const getOptions = (input) => {
-          console.log("Input: " + input);
-
-          return fetch('http://10.10.120.131:5000/name_options', {
-                body: JSON.stringify({"name":input}),
-                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: '*omit', // include, same-origin, *omit
-                headers: {
-                  'user-agent': 'Mozilla/4.0 MDN Example',
-                  'content-type': 'application/json'
-                },
-                method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                // mode: 'no-cors', // no-cors, cors, *same-origin
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-                redirect: 'follow', // *manual, follow, error
-                referrer: 'no-referrer', // *client, no-referrer
-              })
-              .then(res => res.json())
-              .catch(error => console.error('Error:', error))
-              .then(response => {
-                  var result = new Array();
-                  for (var el in response) {
-                      result.push({value:response[el][0], label:response[el][0]});
-                      console.log(response[el][0]);
-                  }
-                  console.log(response);
-                  this.setState({
-                      loaded: result,
-                    })
-                  return { options: result };
-    });
-        }
-      // const { selectedOption } = this.state;
-      // const value = selectedOption && selectedOption.value;
-
-      return (
-              <Async
->>>>>>> 60176d8d40e2880452f1335ef857ee0afa4515e0
                 name="form-field-name"
                 placeholder="Select name of the meds"
                 value={value}
                 onChange={this.handleChange}
-<<<<<<< HEAD
                 options={[{'value': 'ПАГАМАКС', 'label': 'ПАГАМАКС'},
                  {'value': 'ПАЙЗИНА', 'label': 'ПАЙЗИНА'}, {'value': 'ПАКЛІВІСТА', 'label': 'ПАКЛІВІСТА'},
                   {'value': 'ПАКЛІМЕДАК', 'label': 'ПАКЛІМЕДАК'}, {'value': 'ПАКЛІТАКСЕЛ', 'label': 'ПАКЛІТАКСЕЛ'},
@@ -108,10 +48,5 @@ export default class NameInput extends Component{
 
               />
           </div>);
-=======
-                loadOptions={getOptions}
-              />
-      );
->>>>>>> 60176d8d40e2880452f1335ef857ee0afa4515e0
     }
   }
